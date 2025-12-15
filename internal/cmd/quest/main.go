4e0c3b198e0ace0da4f5441eab1168b40fe76d45
Joshua Scarsbrook <jscarsbrook@gmail.com>
2025-12-15 16:53:59 +1000

    virtio: fixes bugs blocking bringup

--- internal/cmd/quest/main.go
+++ internal/cmd/quest/main.go
@@ -1935,7 +1935,7 @@ func RunExecutable(path string) error {
 		initx.WithDeviceTemplate(virtio.FSTemplate{
 			Tag:     "bringup",
 			Backend: vfs.NewVirtioFsBackend(),
-			// Arch:    hv.Architecture(),
+			Arch:    hv.Architecture(),
 		}),
 	)
 	if err != nil {
