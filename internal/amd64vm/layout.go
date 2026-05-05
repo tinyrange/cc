6cd3eed3875eafadeb1e1fdbccf89cb5d9fc7c47
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-05-05 20:36:30 +1000

    Revert "Use high IOAPIC GSIs for amd64 virtio mmio"
    This reverts commit 8279357eb4b093cf2e2c81b0807f72f7b5194868.

--- internal/amd64vm/layout.go
+++ internal/amd64vm/layout.go
@@ -5,22 +5,22 @@ import "j5.nz/cc/internal/vmruntime"
 const (
 	RootFSBase = 0xd0004000
 	RootFSSize = 0x1000
-	RootFSIRQ  = 16
+	RootFSIRQ  = 5
 
 	VsockBase = 0xd0005000
 	VsockSize = 0x1000
-	VsockIRQ  = 17
+	VsockIRQ  = 6
 
 	RNGBase = 0xd0006000
 	RNGSize = 0x1000
-	RNGIRQ  = 18
+	RNGIRQ  = 7
 
 	NetBase = 0xd0007000
 	NetSize = 0x1000
-	NetIRQ  = 19
+	NetIRQ  = 8
 
 	ShareFSBase = 0xd0008000
-	ShareFSIRQ  = 20
+	ShareFSIRQ  = 9
 	FSStride    = 0x1000
 
 	RootFSTag   = vmruntime.RootFSTag
