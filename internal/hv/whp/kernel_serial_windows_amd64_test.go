a430561be8c8d3d8921f0ea2b039267aef01976b
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-05-29 02:14:08 +1000

    Open console in tiny WHP init

--- internal/hv/whp/kernel_serial_windows_amd64_test.go
+++ internal/hv/whp/kernel_serial_windows_amd64_test.go
@@ -74,21 +74,29 @@ func tinyAMD64InitELF(message string) []byte {
 		base      = uint64(0x400000)
 		headerLen = 64 + 56
 	)
+	path := []byte("/dev/console\x00")
 	msg := []byte(message)
 	code := []byte{
+		0xb8, 0x01, 0x01, 0x00, 0x00, // mov eax, SYS_openat
+		0xbf, 0x9c, 0xff, 0xff, 0xff, // mov edi, AT_FDCWD
+		0x48, 0x8d, 0x35, 0, 0, 0, 0, // lea rsi, [rip+path]
+		0xba, 0x02, 0x00, 0x00, 0x00, // mov edx, O_RDWR
+		0x45, 0x31, 0xd2, // xor r10d, r10d
+		0x0f, 0x05, // syscall
+		0x48, 0x89, 0xc7, // mov rdi, rax
 		0xb8, 0x01, 0x00, 0x00, 0x00, // mov eax, SYS_write
-		0xbf, 0x01, 0x00, 0x00, 0x00, // mov edi, STDOUT_FILENO
 		0x48, 0x8d, 0x35, 0, 0, 0, 0, // lea rsi, [rip+msg]
 		0xba, byte(len(msg)), byte(len(msg) >> 8), byte(len(msg) >> 16), byte(len(msg) >> 24), // mov edx, len
 		0x0f, 0x05, // syscall
 		0xf3, 0x90, // pause
 		0xeb, 0xfc, // jmp pause
 	}
-	msgOffset := headerLen + len(code)
-	leaNext := headerLen + 17
-	binary.LittleEndian.PutUint32(code[13:17], uint32(msgOffset-leaNext))
+	pathOffset := headerLen + len(code)
+	msgOffset := pathOffset + len(path)
+	binary.LittleEndian.PutUint32(code[13:17], uint32(pathOffset-(headerLen+17)))
+	binary.LittleEndian.PutUint32(code[38:42], uint32(msgOffset-(headerLen+42)))
 
-	filesz := uint64(headerLen + len(code) + len(msg))
+	filesz := uint64(headerLen + len(code) + len(path) + len(msg))
 	elf := make([]byte, filesz)
 	copy(elf[0:16], []byte{0x7f, 'E', 'L', 'F', 2, 1, 1})
 	binary.LittleEndian.PutUint16(elf[16:18], 2)    // ET_EXEC
@@ -110,6 +118,7 @@ func tinyAMD64InitELF(message string) []byte {
 	binary.LittleEndian.PutUint64(ph[48:56], 0x1000)
 
 	copy(elf[headerLen:], code)
+	copy(elf[pathOffset:], path)
 	copy(elf[msgOffset:], msg)
 	return elf
 }
